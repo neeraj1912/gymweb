03933dd24443031e2f384086619d2460f4240b7c
Neeraj Choudhary <nc28778@gmail.com>
2025-11-11 13:14:26 +0530

    build ok: remove PWA + prod build

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
 import type { NextConfig } from "next";
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  register : true,
-  cacheOnFrontEndNav:true,
-  aggressiveFrontEndNavCaching:true,
-  reloadOnOnline:true,
-  swcMinify:true,
-  disable:false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["*"],
+  },
 };
-module.exports = withPWA(nextConfig);
 
-
-
+export default nextConfig;
